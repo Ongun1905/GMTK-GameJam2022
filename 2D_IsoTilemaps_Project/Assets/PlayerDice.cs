3dd54e20d4658af53aa653f7af708f242553074b
Ongun1905 <ongunkorkmaz@hotmail.com>
2022-07-16 17:23:51 +0200

    Update PlayerDice.cs

--- 2D_IsoTilemaps_Project/Assets/PlayerDice.cs
+++ 2D_IsoTilemaps_Project/Assets/PlayerDice.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class PlayerDice : MonoBehaviour
 {
+    private Sprite[] diceSides;
+    private SpriteRenderer rend;
+    
     // Start is called before the first frame update
     void Start()
     {
+        rend = GetComponent<SpriteRenderer>();
         
     }
 
